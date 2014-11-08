af2a68fd15089cbddda8d0d74776b39cd1c6cdac
Clayton Albachten <clay.reed.a@gmail.com>
2014-11-08 15:11:20 -0500

    Remove hello_world action & change root rule

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def hello_world 
-    greet = ['hey', 
-             'yo', 
-             'hello', 
-             'hola', 
-             'hello'].sample.capitalize 
-    render :text => "#{greet}, World!" 
-  end 
 end
